761f57c1a5a437e5e9d889c68bb5567776568047
David Gutierrez <david.gutierrez@liferay.com>
2021-12-02 23:49:22 +0100

    Refactor to use new implemented class

---
+++ Day1/src/main/java/adventofcode/day1/Day1.java
@@ -0,0 +1,12 @@
+package main.java.adventofcode.day1;
+
+import main.java.adventofcode.common.SonarSweep;
+
+import java.io.IOException;
+
+public class Day1 {
+    public static void main(String[]args) {
+
+        System.out.println("The final result of PROBLEM 1 is: " + SonarSweep.measurementIncreases("data.txt"));
+    }
+}
