503f0c465e55f9a569e41f551df0eeb0a312239e
David Gutierrez <david.gutierrez@liferay.com>
2021-12-03 23:28:09 +0100

    Day 2 Part II

--- Day1/src/main/java/adventofcode/common/Submarine.java
+++ Day1/src/main/java/adventofcode/common/Submarine.java
@@ -36,4 +36,37 @@ public class Submarine {
 
         return horizontalMovement * depth;
     }
+
+    public static int PilotManual(String path) {
+        int horizontalMovement = 0;
+        int depth = 0;
+        int aim = 0;
+
+        FileReaderClass fileReader = new FileReaderClass(path);
+        List<String> depthMeasures = fileReader.readStrings();
+
+        for (String depthMeasure : depthMeasures) {
+
+            String[] splitLine = depthMeasure.split(" ");
+            String instruction = splitLine[0];
+            int distance = Integer.parseInt(splitLine[1]);
+            switch (instruction) {
+                case "forward":
+                    horizontalMovement += distance;
+                    depth += aim * distance;
+                    break;
+                case "down":
+                    aim += distance;
+                    break;
+                case "up":
+                    aim -= distance;
+                    break;
+                default:
+                    System.out.println("no match line");
+                    break;
+            }
+        }
+
+        return horizontalMovement * depth;
+    }
 }
--- Day1/src/main/java/adventofcode/day1/Day2.java
+++ Day1/src/main/java/adventofcode/day1/Day2.java
@@ -5,8 +5,10 @@ import main.java.adventofcode.common.Submarine;
 public class Day2 {
     public static void main(String[]args) {
 
-        int distance = Submarine.Pilot("data_day2.txt");
-        System.out.println("The result of single-measurement is: " + distance);
+        int distance_partI = Submarine.Pilot("data_day2.txt");
+        System.out.println("The result of part I is: " + distance_partI);
 
+        int distance_partII = Submarine.PilotManual("data_day2.txt");
+        System.out.println("The result of part II is: " + distance_partII);
     }
 }
